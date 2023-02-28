4384dceb8fea0f2776c31dafe6e37705f06b7301
DenFedor <fedorenko.wo@gmail.com>
2023-02-28 14:57:14 +0200

    Update paths.js

--- src/router/paths.js
+++ src/router/paths.js
@@ -1,7 +1,7 @@
 export const PAGE_NAMES = {
     home: '/goit-react-hw-05-movies/',
     movies: 'movies',
-    movie: '/movies/:id',
+    movie: 'movies/:id',
     cast: 'cast',
     reviews: 'reviews',
 }
